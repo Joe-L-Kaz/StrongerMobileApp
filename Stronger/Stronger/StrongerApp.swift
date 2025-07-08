37f6f0e439dd772d9eca5bcc8183ef13636491fd
Joseph <joseph.lobokazinczi@hotmail.com>
2025-07-08 19:41:59 +0100

    implementing auth front end

---
+++ Stronger/Stronger/StrongerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongerApp.swift
+//  Stronger
+//
+//  Created by Joseph Lobo-Kazinczi on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct StrongerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
