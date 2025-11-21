c9e8c22f8c09c1c9219ae9c55e15d2774c7ba4d2
Joseph <joseph.lobokazinczi@hotmail.com>
2025-11-21 18:47:56 +0000

    rename file

---
+++ Stronger/Stronger/Features/Exercise/ExerciseDetailsView.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseDetailsView.swift
+//  Stronger
+//
+//  Created by Joseph Lobo-Kazinczi on 21/11/2025.
+//
+
+import SwiftUI
+
+struct ExerciseDetailsView : View {
+    let exercise: ExerciseResponse
+    var body : some View {
+        VStack (spacing: 10) {
+            
+        }
+    }
+}
