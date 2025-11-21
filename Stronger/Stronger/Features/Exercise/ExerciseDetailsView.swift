79324488433db13214fc6cedd04cd7e76620ce48
Joseph <joseph.lobokazinczi@hotmail.com>
2025-11-21 19:48:29 +0000

    started exercise info sheet

--- Stronger/Stronger/Features/Exercise/ExerciseDetailsView.swift
+++ Stronger/Stronger/Features/Exercise/ExerciseDetailsView.swift
@@ -11,7 +11,54 @@ struct ExerciseDetailsView : View {
     let exercise: ExerciseResponse
     var body : some View {
         VStack (spacing: 10) {
+            Text(exercise.name)
+                .font(.title)
+                .fontWeight(.bold)
+                .lineLimit(1)
+                .truncationMode(.tail)
             
+            AsyncImage(url: URL(string: "http://localhost:5020/images/" + (exercise.imagePath ?? ""))) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 250, height: 200)
+            .clipped()
+            .cornerRadius(8)
+            
+            Text(exercise.description)
+            
+            VStack(spacing: 5) {
+                Text("Muscle Groups")
+                    .fontWeight(.bold)
+                    .font(.system(size: 18))
+                
+                Text("back")
+                
+                if(exercise.secondaryMuscleGroup != nil){
+                    Text(exercise.secondaryMuscleGroup!.rawValue)
+                        .fontWeight(.bold)
+                        .font(.system(size: 18))
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding()
     }
+}
+
+#Preview {
+    ExerciseDetailsView(exercise: ExerciseResponse(
+        id: 1,
+        name: "Some Plan",
+        description: "First do this, then do that, finally do that.",
+        imagePath: "SomePath",
+        primaryMuscleGroup: MuscleGroup.back,
+        secondaryMuscleGroup: MuscleGroup.bicep,
+        exerciseType: ExerciseType.strength,
+        forceType: ForceType.push
+    ))
 }
